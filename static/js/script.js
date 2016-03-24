f4ef9bff94a6d76ae93d772ab5b6f98840c6b13b
NEAVES <bn318m@att.com>
2016-03-24 17:07:33 -0400

    Added primitive autocomplete w/jquery for keyfield

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,35 @@
 // Class to represent a row in the seat reservations grid
+
+$(function() {
+   var availableTags = [
+     "ActionScript",
+     "AppleScript",
+     "Asp",
+     "BASIC",
+     "C",
+     "C++",
+     "Clojure",
+     "COBOL",
+     "ColdFusion",
+     "Erlang",
+     "Fortran",
+     "Groovy",
+     "Haskell",
+     "Java",
+     "JavaScript",
+     "Lisp",
+     "Perl",
+     "PHP",
+     "Python",
+     "Ruby",
+     "Scala",
+     "Scheme"
+   ];
+   $( "#KeyGuess" ).autocomplete({
+     source: availableTags
+   });
+ });
+
 function KeyValue(myKey, myValue) {
     var self = this;
     self.myKey=myKey;
