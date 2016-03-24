9c48a06ea919dcc7829e94605624f53ea1d0fc03
NEAVES <bn318m@att.com>
2016-03-24 12:34:13 -0400

    Added test version of knockout functionality

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,22 @@
- function AppViewModel() {
-    this.firstName = "Bert";
-    this.lastName = "Bertington";
+// Class to represent a row in the seat reservations grid
+function KeyValue(name, initialMeal) {
+    var self = this;
+    self.name = name;
+}
+// Overall viewmodel for this screen, along with initial state
+function KVPairsViewModel() {
+    var self = this;
+
+    // Editable data
+    self.pairs = ko.observableArray([
+        new KeyValue("")
+    ]);
+
+    // Operations
+    self.addKVPair = function() {
+        self.pairs.push(new KeyValue(""));
+    }
+    self.removePair = function(pair) { self.pairs.remove(pair) }
 }
 
-// Activates knockout.js
-ko.applyBindings(new AppViewModel());
+ko.applyBindings(new KVPairsViewModel());
