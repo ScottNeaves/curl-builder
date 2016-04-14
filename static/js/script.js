6ebb9e2faceb6c3028d55e590362f227f28d94f7
Brian Neaves <brian.g.neaves@gmail.com>
2016-04-14 12:01:23 -0400

    Fixed bug relating to json remove whitespace in curl command. Works correctly now.

--- static/js/script.js
+++ static/js/script.js
@@ -3,10 +3,6 @@ editor.setTheme("ace/theme/chrome")
 editor.getSession().setMode("ace/mode/javascript");
 editor.setOption("showPrintMargin", false)
 var editorContent = ""
-editor.getSession().on('change', function(e) {
-  editorContent = editor.getValue()
-  editorContent = JSON.stringify(JSON.parse(editorContent))
-});
 
 var AnimalDict = {
   "dog": ['terrier', 'Schnauzer', 'great-dane', 'beagle'],
@@ -17,6 +13,12 @@ var AnimalDict = {
 
 var methodTypes = ['GET', 'PUT', 'POST', 'DELETE']
 var editorModes = ['text', 'json', 'xml']
+
+/*var editorModes = [
+   { value: 'text', name: 'Plain Text' },
+   { value: 'json', name: 'JSON' },
+   { value: 'xml', name: 'XML' }
+]*/
 
 function ViewModel() {
   var self = this;
@@ -31,6 +33,16 @@ function ViewModel() {
   self.editorMode.subscribe(function() {
     editor.getSession().setMode("ace/mode/" + self.editorMode())
   });
+
+  editor.getSession().on('change', function(e) {
+    editorContent = editor.getValue()
+      //editorContent = JSON.stringify(JSON.parse(editorContent))
+    if (self.editorMode() == 'json') {
+      editorContent = editorContent.replace(/[ \n\t]/g,'');
+      console.log(editorContent)
+    }
+  });
+
   self.headers = ko.observableArray([{
     key: ko.observable(""),
     value: ko.observable("")
@@ -40,7 +52,7 @@ function ViewModel() {
     this.queryParameters = "?"
     for (var i = 0; i < self.queryParams().length; i++) {
       this.queryParameters = this.queryParameters + self.queryParams()[i].key() + "=" + self.queryParams()[i].value()
-      if(i < self.queryParams().length - 1){
+      if (i < self.queryParams().length - 1) {
         this.queryParameters = this.queryParameters + "&"
       }
     }
@@ -49,7 +61,8 @@ function ViewModel() {
     for (var i = 0; i < self.headers().length; i++) {
       this.headers = this.headers + " --header \"" + self.headers()[i].key() + ": " + self.headers()[i].value() + "\""
     }
-    return "curl --verbose " + this.headers + " --data \"" + editorContent + "\"--request \"" + self.methodType() + "\" \"" + self.url() + this.queryParameters + "\"" })
+    return "curl --verbose " + this.headers + " --data \"" + editorContent + "\"--request \"" + self.methodType() + "\" \"" + self.url() + this.queryParameters + "\""
+  })
 
 
 
