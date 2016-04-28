a463c003fa962ac3fd5dc0819a31b415fa58d6e9
Brian Neaves <brian.g.neaves@gmail.com>
2016-04-28 15:44:42 -0400

    Added Mongo capability to app.py. Right now the object ID of an empty test post is displayed on the page at the top.

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 from flask import Flask
 from flask import render_template
-
+from flask.ext.pymongo import PyMongo
+from datetime import datetime
 app = Flask(__name__)
+mongo = PyMongo(app)
+
+post = {"headers": {},
+              "qParams": {},
+              "username": "john",
+              "password": "doe",
+              "dataType": "JSON",
+              "data": "{testdata: hi}",
+              "method": "GET",
+              "URL": "www.google.com",
+              "date": datetime.utcnow()}
+
 
 @app.route('/')
 def hello_world():
-    return render_template("index.html")
+    helloDb = mongo.db.items.insert_one(post).inserted_id
+    return render_template("index.html", post=helloDb)
 
 if __name__ == '__main__':
     app.run(debug=True)
