d51fcdcac8566edc588be91cbc8a7f3433e4221b
Brian Neaves <brian.g.neaves@gmail.com>
2016-05-02 15:54:32 -0400

    Added success.html to be redirected to when user clicks save. Attempted to implement redirect funcitonality, not working right now

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, send_file
 from flask.ext.pymongo import PyMongo
 from flask import request, jsonify
 from datetime import datetime
+from random import randint
+import json
+from flask import redirect
 app = Flask(__name__)
 mongo = PyMongo(app)
+
 
 @app.route('/', methods=['POST', 'GET'])
 def hello_world():
@@ -14,11 +18,20 @@ def hello_world():
 @app.route('/saveSnippet', methods=['POST'])
 def saveSnippet():
     data = request.get_json()
+    data['randInt'] = randint(100000, 999999)
+    #Add random code
+    #randIdentifier = randint(100000, 999999)
+    #data.randIdentifier = randIdentifier
     print data
     curlCommandDb = mongo.db.savedCurls.insert_one(data).inserted_id
     print curlCommandDb
     print mongo.db.savedCurls.count()
-    return 'hi'
+    return redirect("http://127.0.0.1:5000/success", code=302) #http://127.0.0.1:5000/success
+
+@app.route('/success', methods=['GET'])
+def success():
+    print "Im here"
+    return send_file("templates/success.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
