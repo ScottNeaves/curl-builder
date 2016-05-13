0a959188440483f34dc819412cb503abb98f02d9
Brian Neaves <brian.g.neaves@gmail.com>
2016-05-13 13:49:19 -0400

    Fixed issue where incoming json information from snippet retreival did not show up right in headers and query parameters

--- static/js/script.js
+++ static/js/script.js
@@ -100,8 +100,7 @@ function ViewModel() {
       error: function() {
         console.log("error occurred")
       }
-    });
-
+    })
   }
 
   self.curlCommand = ko.computed(function() {
@@ -195,16 +194,18 @@ function ViewModel() {
           self.username(curl.username)
           self.password(curl.password)
           console.log(curl.headers.length)
+          self.headers().pop()
           for (var i = 0; i < curl.headers.length; i++) {
-            self.headers().push({
+            self.headers.push({
               key: ko.observable(curl.headers[i].key),
               value: ko.observable(curl.headers[i].value)
             })
-            console.log('key is ' + curl.headers[i].key)
-            console.log('value is ' + curl.headers[i].value)
+            console.log('hdrs key is ' + curl.headers[i].key)
+            console.log('hdrs value is ' + curl.headers[i].value)
           }
+          self.queryParams().pop()
           for (var i = 0; i < curl.queryParameters.length; i++) {
-            self.queryParams().push({
+            self.queryParams.push({
               key: ko.observable(curl.queryParameters[i].key),
               value: ko.observable(curl.queryParameters[i].value)
             })
